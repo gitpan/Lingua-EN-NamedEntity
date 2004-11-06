4a654836d476d67c83f633fbe09896c02cf34f25
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2004-11-06 21:30:27 +0000

    Import of AMBS/Lingua-EN-NamedEntity-1.5 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-NamedEntity
    gitpan-cpan-version:      1.5
    gitpan-cpan-path:         AMBS/lingua/Lingua-EN-NamedEntity-1.5.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- NamedEntity.pm
+++ NamedEntity.pm
@@ -25,7 +25,7 @@ require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(extract_entities);
 
-our $VERSION = '1.3';
+our $VERSION = '1.5';
 
 
 # Regexps for constructing capitalised sequences
